9ac4b1226aaa89eeb9381aa654c16cd38808f946
Nelson Lu <nelkalmlu@gmail.com>
2022-11-26 13:10:26 -0800

    initial states for reports setup

---
+++ client/src/utils/reportFormOptions.js
@@ -0,0 +1,17 @@
+export const PROGRAM_TYPE = ["Art", "Nature", "Neighborhood", "Admin"];
+
+export const PROGRAM_SUBTYPE = [
+  "EcoArts",
+  "Summer Youth Program",
+  "Youngstown Arts",
+  "Restoration",
+  "Wetlands",
+  "Restorative Justice",
+  "Environmental Justice",
+  "Resident Services",
+  "Family Connections & Youth Development",
+];
+
+export const PROGRAM_STATUS = ["new", "ongoing", "completed"];
+
+export const EXPECTATION_EVAL_STAFF = [1, 2, 3, 4, 5];
